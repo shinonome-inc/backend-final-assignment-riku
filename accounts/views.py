a357d668553294ec72046393aee7dba8d9e31319
matapaku <matapaku0119@gmail.com>
2024-08-20 15:51:37 +0900

    サインアップ機能の実装

--- accounts/views.py
+++ accounts/views.py
@@ -1,1 +1,28 @@
-# from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+
+from .forms import SignupForm
+
+
+class SignupView(CreateView):
+    form_class = SignupForm
+    template_name = "accounts/signup.html"
+    success_url = reverse_lazy("tweets:home")
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        username = form.cleaned_data["username"]
+        password = form.cleaned_data["password1"]
+        
+        user = authenticate(self.request, userame=username, password=password)
+        login(self.request, user)
+        return response
+    
+    # def form_valid(self, form):
+    #     username = form.cleaned_data["username"]
+    #     print(form.cleaned_data)
+    #     password = form.cleaned_data["password"]
+    #     user = authenticate(self.request, username=username, passward=password)
+    #     login(self.request, user)
+    #     return = super().form_valid(form)
